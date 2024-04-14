170ccbabc9301554a61f1cb5ed935f14719922b2
glckfndr <obulan65@gmail.com>
2024-04-14 15:16:49 +0300

    feat: add cashing

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,11 +1,25 @@
 import Link from "next/link";
 import React from "react";
 
-const UsersPage = () => {
+interface User {
+  id: number;
+  name: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    next: { revalidate: 10 },
+  });
+  const users: User[] = await res.json();
   return (
     <div>
-      <h2>Users Page</h2>
+      <h2>Users: </h2>
       <Link href="users/new">New User Page</Link>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
