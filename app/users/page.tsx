96eb583746a39dcc119f4c76ac143efd72e93645
glckfndr <obulan65@gmail.com>
2024-04-22 21:04:37 +0300

    feat: add UserTable, UserDetailPage, PhotoPage

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,26 +1,14 @@
 import Link from "next/link";
 import React from "react";
+import UserTable from "./UserTable";
 
-interface User {
-  id: number;
-  name: string;
-}
-
-const UsersPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-    next: { revalidate: 10 },
-  });
-  const users: User[] = await res.json();
+const UsersPage = () => {
   return (
     <div>
       <h2>Users: </h2>
       <Link href="users/new">New User Page</Link>
       <p>{new Date().toLocaleTimeString()}</p>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      <UserTable />
     </div>
   );
 };
