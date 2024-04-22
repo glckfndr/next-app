91a98e00300768a7afb4e95e057da92938c6767c
glckfndr <obulan65@gmail.com>
2024-04-22 21:03:52 +0300

    feat: using daisyUI

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -3,7 +3,10 @@ import React from "react";
 
 const AddToCart = () => {
   return (
-    <button onClick={() => console.log("Button is pressed")}>
+    <button
+      className="btn btn-primary"
+      onClick={() => console.log("Button is pressed")}
+    >
       Add to cart
     </button>
   );
